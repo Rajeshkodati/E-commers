a3d0227f0ae3724c7c18a99f9f46192f19a78d88
rajesh <rajesh.kodati@gmail.com>
2019-02-19 16:08:38 +0530

    create data load

--- app.js
+++ app.js
@@ -27,40 +27,129 @@ function renderData(data){
    
 }
  */
+ /* for(let i=0; i<type.length; i++){
+       if(type[i] === '.women'){
+        $( type[i]).click(function(){
+            $.ajax({
+                type:"GET",
+                url:"http://localhost:3000/mommy",
+                success:function(outerData){
+    
+                   renderData(outerData);
+                }
+            })
+        });
+       }
+     }
+ */ let type;
 $(document).ready(function(){
-    $.ajax({
-        type:"GET",
-        url:"http://localhost:3000/mommy",
-        success:function(outerData){
-           renderData(outerData);
-        }
-    })
+     type = ['men','women','baby','all'];
+    // $.ajax({
+    //     type:"GET",
+    //     url:"http://localhost:3000/mommy",
+    //     success:function(outerData){
+    //        renderData(outerData);
+    //     }
+    // })
     $('.inner_menu').click(function(){
     $('#inner_nav').css({display:'block'});
     });
-    $('.innernav').click(function(){
+       
+    $('.men').click(function(){
         $.ajax({
             type:"GET",
             url:"http://localhost:3000/mommy",
             success:function(outerData){
-               renderData(outerData);
+
+                renderMenData(outerData);
             }
         })
     });
+    $('.women').click(function(){
+        $.ajax({
+            type:"GET",
+            url:"http://localhost:3000/mommy",
+            success:function(outerData){
 
+                renderWomenData(outerData);
+            }
+        })
+    });
+    $('.all').click(function(){
+        console.log("this");
+        $.ajax({
+            type:"GET",
+            url:"http://localhost:3000/mommy",
+            success:function(outerData){
+
+                renderAllData(outerData);
+            }
+        })
+    });
+    $('.baby').click(function(){
+        console.log("this");
+        $.ajax({
+            type:"GET",
+            url:"http://localhost:3000/mommy",
+            success:function(outerData){
+
+                renderBabyData(outerData);
+            }
+        })
+    });
 })
-function renderData(data,event){
-    console.log(data.men)
-     data.men.map(loadImage => 
-        $('#inner_cont').append(`<div id=${loadImage.id}><img src='${loadImage.img}'></div>`));
-        $('.innernav').off('click');
-       
-    
-    // for(let i=0; i<data.length; i++){
-    //     console.log(data.length)
-    //         $('#inner_cont').append(`<div id=${data[i].id}><img src='${data[i].img}'></div>`); 
-    // $('.innernav').hide(200);
-                         
-    // }
-    
-} 
+
+function renderWomenData(data,event){
+    $('.display').show();
+    $('#banner').hide();
+    $('#head_cont').hide();
+    // console.log(type[0])
+    data.women.map(loadImage =>
+            $('#inner_cont').append(`<div class='display'><img src='${loadImage.img}'></div>`));
+           $('.women').off('click');
+           $('.men_display').hide();
+           $('.baby_display').hide();
+           $('.all_display').hide(); 
+           
+}
+function renderMenData(data,event){
+    $('.men_display').show();
+    $('#banner').hide();
+    $('#head_cont').hide();
+    // console.log(type[0])
+    data.men.map(loadImage => 
+            $('#inner_cont').append(`<div class='men_display'><img src='${loadImage.img}'></div>`));
+           $('.men').off('click');  
+           $('.display').hide();
+           $('.baby_display').hide();
+           $('.all_display').hide();
+}
+function renderBabyData(data,event){
+    $('.baby_display').show();
+    $('#banner').hide();
+    $('#head_cont').hide();
+    // console.log(type[0])
+    data.baby.map(loadImage => 
+            $('#inner_cont').append(`<div class='baby_display'><img src='${loadImage.img}'></div>`));
+           $('.baby').off('click');  
+           $('.men_display').hide();
+           $('.display').hide();
+           $('.all_display').hide();
+}
+function renderAllData(data,event){
+    // console.log(type[0])
+
+    $('.all_display').show();
+    $('#banner').hide();
+    $('#head_cont').hide();
+    data.all.map(loadImage => 
+            $('#inner_cont').append(`<div class='all_display'><img src='${loadImage.img}'></div>`));
+           $('.all').off('click');  
+           $('.men_display').hide();
+           $('.display').hide();
+           $('.baby_display').hide();
+}
+
+$('#inner_cont').on('click','.display',function(){
+    alert('')
+});
